9af94ca1fd9b7a9c54f73f3cd5563eb8da75dab4
camon <camon85@gmail.com>
2017-03-14 01:20:38 +0900

    pid 생성

--- src/main/java/com/camon/App.java
+++ src/main/java/com/camon/App.java
@@ -2,6 +2,7 @@ package com.camon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,5 +19,9 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(App.class, args);
+
+    SpringApplication springApplication = new SpringApplication(App.class);
+    springApplication.addListeners(new ApplicationPidFileWriter());
+    springApplication.run(args);
   }
 }
